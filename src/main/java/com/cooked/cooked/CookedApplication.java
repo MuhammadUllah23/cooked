f9d0eb440aa3047b4723b93790f8ad18874ed15e
muhammadullah <muhammadhullah23@gmail.com>
2023-09-03 22:19:06 -0400

    First commit

---
+++ src/main/java/com/cooked/cooked/CookedApplication.java
@@ -0,0 +1,21 @@
+package com.cooked.cooked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CookedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookedApplication.class, args);
+	}
+
+	@GetMapping("/root")
+	public String apiRoot() {
+		return "Hello, World!";
+	}
+
+}
