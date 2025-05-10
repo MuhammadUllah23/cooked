34614b6b097f63f0972d01d4f18b9435e1339830
M.Ullah <muhammadhullah23@gmail.com>
2025-05-09 23:45:48 -0400

    Initial Commit

---
+++ cooked-backend/src/main/java/com/example/cooked_backend/CookedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cooked_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookedBackendApplication.class, args);
+	}
+
+}
---
+++ cooked-backend/src/test/java/com/example/cooked_backend/CookedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cooked_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
