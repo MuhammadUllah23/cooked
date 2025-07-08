7f336f68f2fe1488a967bebdee4853a325388971
M.Ullah <muhammadhullah23@gmail.com>
2025-07-08 01:45:09 -0400

    Home page CSS

--- cooked-frontend/src/components/Home.js
+++ cooked-frontend/src/components/Home.js
@@ -5,19 +5,20 @@ import HomeNavBar from "./HomeNavBar";
 
 function Home() {
     return (
-        <div className="home">
-        <HomeNavBar />
+        <div>
+            <HomeNavBar />
+            <div className="home">
+                <main className="hero">
+                    <h1>hi</h1>
+                        <h2>Manage your small business inventory with ease</h2>
+                        <p>Cooked helps small businesses track, manage, and organize their stock effortlessly.</p>
+                        <a className="cta-button" href="/register">Get Started</a>
+                </main>
 
-        <main className="hero">
-        <h1>hi</h1>
-            <h2>Manage your small business inventory with ease</h2>
-            <p>Cooked helps small businesses track, manage, and organize their stock effortlessly.</p>
-            <a className="cta-button" href="/register">Get Started</a>
-        </main>
-
-        <footer>
-            &copy; {new Date().getFullYear()} Cooked
-        </footer>
+                <footer>
+                    &copy; {new Date().getFullYear()} Cooked
+                </footer>
+            </div>
         </div>
     );
 }
