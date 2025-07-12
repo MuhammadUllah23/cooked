a9b42de7df89106cda0f4751bc54ae22b29e83bf
M.Ullah <muhammadhullah23@gmail.com>
2025-07-12 01:15:21 -0400

    spring boot set up

--- cooked-backend/src/main/java/com/example/cooked_backend/CookedBackendApplication.java
+++ cooked-backend/src/main/java/com/example/cooked_backend/CookedBackendApplication.java
@@ -3,10 +3,14 @@ package com.example.cooked_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class CookedBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+
 		SpringApplication.run(CookedBackendApplication.class, args);
 	}
 
