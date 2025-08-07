9b8e050b78dadebdf521ffed841ff65efcb90e1c
M.Ullah <muhammadhullah23@gmail.com>
2025-08-07 17:32:47 -0400

    Default Business Service Skeleton

---
+++ cooked-backend/src/main/java/com/example/cooked_backend/service/DefaultBusinessService.java
@@ -0,0 +1,30 @@
+package com.example.cooked_backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultBusinessService implements BusinessService {
+
+	@Override
+	public Business getBusinessById(UUID id) {
+		// TODO: implement this method
+		return null;
+	}
+
+	@Override
+	public void deleteBusinessById(UUID id) {
+		// TODO: implement this method
+	}
+
+	@Override
+	public List<Business> getAllBusinessesByUserId(UUID userId) {
+		// TODO: implement this method
+		return null;
+	}
+
+	@Override
+	public Business createUser(Business business) {
+		// TODO: implement this method
+		return null;
+	}
+}
