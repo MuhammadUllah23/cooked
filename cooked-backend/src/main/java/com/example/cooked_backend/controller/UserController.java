20ae01691ca3942c88fd56dae27fa7a5b449f26c
M.Ullah <muhammadhullah23@gmail.com>
2025-08-08 22:17:48 -0400

    create UserController

---
+++ cooked-backend/src/main/java/com/example/cooked_backend/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.cooked_backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.cooked_backend.service.UserService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // CREATE user
+    @PostMapping
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserRequest userRequest) {
+        UserResponse createdUser = userService.createUser(userRequest);
+        return ResponseEntity.ok(createdUser);
+    }
+}
