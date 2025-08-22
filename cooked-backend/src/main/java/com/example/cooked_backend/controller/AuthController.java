7ce258e39501d7bc24dfb84bb8b1b2894681d893
M.Ullah <muhammadhullah23@gmail.com>
2025-08-22 17:36:47 -0400

    Update AuthController.java

--- cooked-backend/src/main/java/com/example/cooked_backend/controller/AuthController.java
+++ cooked-backend/src/main/java/com/example/cooked_backend/controller/AuthController.java
@@ -3,7 +3,7 @@ package com.example.cooked_backend.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.cooked_backend.service.UserService;
+import com.example.cooked_backend.service.DefaultUserService;
 import com.example.cooked_backend.dto.request.UserRequest;
 import com.example.cooked_backend.dto.response.UserResponse;
 
@@ -13,16 +13,16 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/users")
 public class AuthController {
 	
-    private final UserService userService;
+    private final DefaultUserService defaultUserService;
 
-    public AuthController(UserService userService) {
-        this.userService = userService;
+    public AuthController(DefaultUserService defaultUserService) {
+        this.defaultUserService = defaultUserService;
     }
 
     // CREATE user
     @PostMapping
     public ResponseEntity<UserResponse> registerAndLoginUser(@Valid @RequestBody UserRequest userRequest) {
-        UserResponse createdUser = userService.createUser(userRequest);
+        UserResponse createdUser = defaultUserService.createUser(userRequest);
         return ResponseEntity.ok(createdUser);
     }
 }
