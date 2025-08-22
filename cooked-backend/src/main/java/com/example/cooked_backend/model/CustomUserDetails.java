57dc2a26edb14199bd4f725b5eccc5f1d126ca1d
M.Ullah <muhammadhullah23@gmail.com>
2025-08-22 17:01:00 -0400

    Create CustomUserDetails

---
+++ cooked-backend/src/main/java/com/example/cooked_backend/model/CustomUserDetails.java
@@ -0,0 +1,58 @@
+package com.example.cooked_backend.model;
+
+import com.example.cooked_backend.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final UUID id;
+    private final String email;
+    private final String password;
+
+    public CustomUserDetails(User user) {
+        this.id = user.getId();
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email; // Spring will treat email as "username"
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+ 
+}
