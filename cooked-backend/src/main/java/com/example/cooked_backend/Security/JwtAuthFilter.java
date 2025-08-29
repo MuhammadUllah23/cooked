add2f05aa4756428ca8be21e88d679a979312cac
M.Ullah <muhammadhullah23@gmail.com>
2025-08-28 22:04:32 -0400

    cerate doFilterInternal in JwtAuthFilter

--- cooked-backend/src/main/java/com/example/cooked_backend/Security/JwtAuthFilter.java
+++ cooked-backend/src/main/java/com/example/cooked_backend/Security/JwtAuthFilter.java
@@ -1,9 +1,13 @@
 package com.example.cooked_backend.Security;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import com.example.cooked_backend.service.CustomUserDetailsService;
+import com.example.cooked_backend.model.CustomUserDetails;
 import com.example.cooked_backend.util.JwtUtil;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,6 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
 public class JwtAuthFilter extends OncePerRequestFilter {
@@ -26,7 +31,27 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
                                     throws ServletException, IOException {
-        // TODO: Implement JWT authentication logic here
+        
+        final String authHeader = request.getHeader("Authorization");
+
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+        }
+
+        final String token = authHeader.substring(7);
+        final UUID userId = jwtUtil.extractUserId(token);
+
+        if (userId != null & SecurityContextHolder.getContext().getAuthentication() == null) {
+            CustomUserDetails userDetails = (CustomUserDetails) customUserDetailsService.loadUserByUsername(jwtUtil.extractEmail(token));
+
+            if (jwtUtil.validateToken(token)) {
+                UsernamePasswordAuthenticationToken authToken = 
+                new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
         filterChain.doFilter(request, response);
     }
 }
