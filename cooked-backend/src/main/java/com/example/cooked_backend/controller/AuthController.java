f1911cee2c201a29722ba0f7fd26b4172ba4467f
M.Ullah <muhammadhullah23@gmail.com>
2025-09-05 23:54:51 -0400

    cleanup warnings

--- cooked-backend/src/main/java/com/example/cooked_backend/controller/AuthController.java
+++ cooked-backend/src/main/java/com/example/cooked_backend/controller/AuthController.java
@@ -1,9 +1,6 @@
 package com.example.cooked_backend.controller;
 
-import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 
@@ -12,20 +9,16 @@ import com.example.cooked_backend.service.DefaultUserService;
 import com.example.cooked_backend.dto.request.LoginRequest;
 import com.example.cooked_backend.dto.request.UserRequest;
 import com.example.cooked_backend.dto.response.AuthResponse;
-import com.example.cooked_backend.dto.response.UserResponse;
-
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 	
     private final DefaultUserService defaultUserService;
-    private final AuthenticationManager authenticationManager;
     private final DefaultAuthService defaultAuthService;
 
-    public AuthController(DefaultUserService defaultUserService, AuthenticationManager authenticationManager, DefaultAuthService defaultAuthService) {
+    public AuthController(DefaultUserService defaultUserService, DefaultAuthService defaultAuthService) {
         this.defaultUserService = defaultUserService;
-        this.authenticationManager = authenticationManager;
         this.defaultAuthService = defaultAuthService;
     }
 
