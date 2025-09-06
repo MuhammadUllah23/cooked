18a6bb2ad7e6dacf354f59197a263ee1eed0c6ea
M.Ullah <muhammadhullah23@gmail.com>
2025-09-05 22:36:54 -0400

    create LoginRequest model

---
+++ cooked-backend/src/main/java/com/example/cooked_backend/dto/request/LoginRequest.java
@@ -0,0 +1,35 @@
+package com.example.cooked_backend.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class LoginRequest {
+
+    @NotNull(message = "Email is required")
+    @Email(message = "Email should be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotNull(message = "Password is required")
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
