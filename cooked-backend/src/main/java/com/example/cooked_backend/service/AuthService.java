a41f3a4b2f4dee4b0b6a3577f084ddcc4e834929
M.Ullah <muhammadhullah23@gmail.com>
2025-09-05 22:54:34 -0400

    update login methods to include createdUser

--- cooked-backend/src/main/java/com/example/cooked_backend/service/AuthService.java
+++ cooked-backend/src/main/java/com/example/cooked_backend/service/AuthService.java
@@ -3,8 +3,9 @@ package com.example.cooked_backend.service;
 import com.example.cooked_backend.dto.request.LoginRequest;
 import com.example.cooked_backend.dto.request.UserRequest;
 import com.example.cooked_backend.dto.response.AuthResponse;
+import com.example.cooked_backend.dto.response.UserResponse;
 
 public interface AuthService {
-    AuthResponse loginUser(UserRequest userRequest);
-    AuthResponse loginUser(LoginRequest loginRequest);    
+    AuthResponse loginUser(UserRequest userRequest, UserResponse createdUser);
+    AuthResponse loginUser(LoginRequest loginRequest, UserResponse createdUser);    
 }
