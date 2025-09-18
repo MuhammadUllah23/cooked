46f196ebed51fb1c25ed6c3d4cd34f520bd36d44
M.Ullah <muhammadhullah23@gmail.com>
2025-09-17 23:00:29 -0400

    drop ip address for privacy concerns

--- cooked-backend/src/main/java/com/example/cooked_backend/model/RefreshToken.java
+++ cooked-backend/src/main/java/com/example/cooked_backend/model/RefreshToken.java
@@ -18,7 +18,7 @@ public class RefreshToken {
     @Column(name = "user_id", updatable = false, nullable = false)
     private UUID userId;
 
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false, updatable = false, unique = true)
     private String token;
 
     @Column(name = "expires_at", nullable = false)
@@ -27,17 +27,13 @@ public class RefreshToken {
     @Column(name = "created_at", nullable = false)
     private OffsetDateTime createdAt = OffsetDateTime.now(ZoneOffset.UTC);
 
-    @Column(name = "ip_address")
-    private String ipAddress;
-
     // Constructors
     public RefreshToken() {}
 
-    public RefreshToken(UUID userId, String token, OffsetDateTime expiresAt, String ipAddress) {
+    public RefreshToken(UUID userId, String token, OffsetDateTime expiresAt) {
         this.userId = userId;
         this.token = token;
         this.expiresAt = expiresAt;
-        this.ipAddress = ipAddress;
         this.createdAt = OffsetDateTime.now(ZoneOffset.UTC);
     }
 
@@ -80,14 +76,6 @@ public class RefreshToken {
 
     public void setCreatedAt(OffsetDateTime createdAt) { 
         this.createdAt = createdAt; 
-    }
-
-    public String getIpAddress() { 
-        return ipAddress; 
-    }
-
-    public void setIpAddress(String ipAddress) { 
-        this.ipAddress = ipAddress; 
     }
     
 }
