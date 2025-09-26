4aae6bdbdd286f5548740b54c6b46f75f7bf9cde
M.Ullah <muhammadhullah23@gmail.com>
2025-09-25 20:57:42 -0400

    create upsert RefreshToken query

--- cooked-backend/src/main/java/com/example/cooked_backend/repository/RefreshTokenRepository.java
+++ cooked-backend/src/main/java/com/example/cooked_backend/repository/RefreshTokenRepository.java
@@ -3,6 +3,9 @@ package com.example.cooked_backend.repository;
 import com.example.cooked_backend.model.RefreshToken;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -13,4 +16,16 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID
     void deleteAllByUserId(UUID userId);
 
     void deleteByUserIdAndDeviceId(UUID userId, UUID deviceId);
+
+    @Modifying
+    @Query(value = """
+        INSERT INTO refresh_tokens (user_id, device_id, token, expires_at, created_at)
+        VALUES (:#{#rt.userId}, :#{#rt.deviceId}, :#{#rt.token}, :#{#rt.expiresAt}, :#{#rt.createdAt})
+        ON CONFLICT (user_id, device_id)
+        DO UPDATE
+        SET token = EXCLUDED.token,
+            expires_at = EXCLUDED.expires_at,
+            created_at = EXCLUDED.created_at
+    """, nativeQuery = true)
+    void upsert(@Param("rt") RefreshToken refreshToken);
 }
