10970b5978f0d280ea60794d090716f38264da3c
M.Ullah <muhammadhullah23@gmail.com>
2025-09-30 23:17:47 -0400

    create publica endpoints constants

---
+++ cooked-backend/src/main/java/com/example/cooked_backend/Security/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.example.cooked_backend.Security;
+
+public class SecurityConstants {
+        public static final String[] PUBLIC_ENDPOINTS = {
+        "/auth/login",
+        "/auth/register",
+        "/auth/refresh"
+    };
+}
