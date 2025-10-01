cbdc87c3a4f521e431e9b5578e8bbf0b10e90ccc
M.Ullah <muhammadhullah23@gmail.com>
2025-09-30 22:43:24 -0400

    Add error code for refresh token

--- cooked-backend/src/main/java/com/example/cooked_backend/exception/ErrorCode.java
+++ cooked-backend/src/main/java/com/example/cooked_backend/exception/ErrorCode.java
@@ -4,7 +4,11 @@ import org.springframework.http.HttpStatus;
 
 public enum ErrorCode {
     USER_ALREADY_EXISTS("User already exists with the following email", HttpStatus.CONFLICT),
-    USER_NOT_FOUND("User not found", HttpStatus.NOT_FOUND);
+    USER_NOT_FOUND("User not found", HttpStatus.NOT_FOUND),
+    INVALID_REFRESH_TOKEN("Invalid refresh token", HttpStatus.UNAUTHORIZED),
+    EXPIRED_REFRESH_TOKEN("Refresh Token is expired", HttpStatus.UNAUTHORIZED)
+    
+    ;
 
     private final String message;
     private final HttpStatus status;
