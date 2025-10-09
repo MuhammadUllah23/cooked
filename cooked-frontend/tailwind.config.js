d9ea00af8f9d20f4893e41302afd74db3f5dff92
M.Ullah <muhammadhullah23@gmail.com>
2025-10-09 14:45:05 -0400

    configure tailwiond paths

--- cooked-frontend/tailwind.config.js
+++ cooked-frontend/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // scans all React components
+    "./public/index.html"
+  ],
   theme: {
     extend: {},
   },
