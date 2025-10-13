adb4418db8114613e60b45f812d01fde019a0fe1
M.Ullah <muhammadhullah23@gmail.com>
2025-10-13 17:22:46 -0400

    Add AuthPage and routing

--- cooked-frontend/src/App.js
+++ cooked-frontend/src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import AuthPage from './pages/AuthPage.tsx';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/auth" element={<Auth />} /> */}
+        <Route path="/auth" element={<AuthPage />} />
         <Route path="*" element={<h1>404 Page Not Found</h1>} />
       </Routes>
     </Router>
---
+++ cooked-frontend/src/pages/AuthPage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { useSearchParams } from "react-router-dom";
+// import LoginForm from "../components/LoginForm";
+// import RegisterForm from "../components/RegisterForm";
+
+const AuthPage = () => {
+  return <div>Auth Page</div>;
+};
+
+export default AuthPage;
