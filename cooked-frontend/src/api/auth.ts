e8def906c92f24da1065334cb3515b57f5df80b5
M.Ullah <muhammadhullah23@gmail.com>
2025-10-20 14:09:30 -0400

    create skeleton for axios auth requests

---
+++ cooked-frontend/src/api/auth.ts
@@ -0,0 +1,58 @@
+import api, { setAccessToken } from "./api";
+
+export enum UserSubscription {
+  FREE = "FREE",
+  PREMIUM = "PREMIUM",
+}
+
+export enum UserRole {
+  USER = "USER",
+}
+
+export interface UserResponse {
+  id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  role: UserRole;
+  subscription: UserSubscription;
+}
+
+export interface AuthResponse {
+  token: string;
+  user: UserResponse;
+  deviceId: string;
+}
+
+export interface RegisterRequest {
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+  deviceId: string;
+}
+
+export interface RefreshResponse {
+  accessToken: string;
+}
+
+export async function registerUser(data: RegisterRequest): Promise<AuthResponse> {
+
+}
+
+export async function loginUser(data: RegisterRequest): Promise<AuthResponse> {
+
+}
+
+export async function logoutUser(data: RegisterRequest): Promise<AuthResponse> {
+
+}
+
+export async function refresh(data: RegisterRequest): Promise<AuthResponse> {
+
+}
