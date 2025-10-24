f0189d64505011e74a0122d111d7e538894d44ff
M.Ullah <muhammadhullah23@gmail.com>
2025-10-24 17:40:25 -0400

    create AuthContext

--- cooked-frontend/src/context/AuthContext.tsx
+++ cooked-frontend/src/context/AuthContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState, useEffect, ReactNode } from "react";
 import { UserResponse } from "../api/auth";
 import { setAccessToken } from "../api/api";
-import { refresh } from "../api/auth";
+import { refresh, logoutUser } from "../api/auth";
 
 interface AuthContextType {
     user: UserResponse | null;
@@ -12,31 +12,65 @@ interface AuthContextType {
     isLoggedIn: boolean;
 }
 
+let logoutSingleton: (() => void) | null = null; 
+
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
     const [user, setUser] = useState<UserResponse | null>(null);
-    const [accessToken, setToken] = useState<string | null>(null);
     const [deviceId, setDeviceId] = useState<string | null>(null);
 
-      useEffect(() => {
+  useEffect(() => {
     const storedUser = localStorage.getItem("user");
     const storedDeviceId = localStorage.getItem("deviceId");
 
     if (storedUser && storedDeviceId) {
       const parsedUser = JSON.parse(storedUser);
       setUser(parsedUser);
-      setDeviceId(storedDeviceId);
+      setDeviceId(storedDeviceId);      
 
       // Try refreshing access token via cookie
-      refresh()
-        .then((newToken) => {
-          setToken(newToken);
-          setAccessToken(newToken);
+      refresh({deviceId: storedDeviceId})
+        .then((res) => {
+          setAccessToken(res.accessToken);
         })
         .catch(() => {
-          logout(); // refresh failed → user not authenticated
+          logoutUser({deviceId: storedDeviceId, global: false}); 
         });
     }
   }, []);
+
+  const login = (user: UserResponse, token: string, deviceId: string) => {
+    setUser(user);
+    setDeviceId(deviceId);
+    localStorage.setItem("user", JSON.stringify(user));
+    localStorage.setItem("deviceId", deviceId);
+    setAccessToken(token);
+  };
+
+  const logout = () => {
+    setUser(null);
+    setDeviceId(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("deviceId");
+    setAccessToken(null);
+    window.location.href = "/login";
+  };
+
+  logoutSingleton = logout;
+
+  const value = {
+    user,
+    deviceId,
+    accessToken: "",
+    login,
+    logout,
+    isLoggedIn: !!user,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
+
+export const callLogout = () => {
+  if (logoutSingleton) logoutSingleton();
+};
