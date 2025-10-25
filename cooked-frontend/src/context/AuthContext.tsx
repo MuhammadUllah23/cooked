e3206c5eca57d61049076c579a746d5aaa3f6e3f
M.Ullah <muhammadhullah23@gmail.com>
2025-10-25 14:53:49 -0400

    add useAuth to AuthContext

--- cooked-frontend/src/context/AuthContext.tsx
+++ cooked-frontend/src/context/AuthContext.tsx
@@ -71,6 +71,14 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
 
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
+
 export const callLogout = () => {
   if (logoutSingleton) logoutSingleton();
 };
