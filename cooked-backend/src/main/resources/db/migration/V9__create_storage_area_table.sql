3ce258f58141fc3e3a5585262fbcc09d35fa3d9e
M.Ullah <muhammadhullah23@gmail.com>
2025-11-10 21:11:34 -0500

    create storage area table

---
+++ cooked-backend/src/main/resources/db/migration/V9__create_storage_area_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS storage_areas (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    stores_id UUID NOT NULL REFERENCES stores(id) ON DELETE CASCADE,
+    name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT unique_store_storage_name UNIQUE (store_id, name)
+);
